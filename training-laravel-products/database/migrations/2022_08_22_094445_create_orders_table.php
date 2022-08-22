b0a923fca9aec3863d23b6826902dd9c8c0c11cc
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-22 17:03:52 +0300

    11 Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/11 created migrations for products, customers, orders and archived products used in orders and seeding for the products table

---
+++ training-laravel-products/database/migrations/2022_08_22_094445_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+//        Schema::create('orders', function (Blueprint $table) {
+//            $table->foreignId('id_product')->references('id')->on('archived_products')->onDelete('cascade');
+//            $table->foreignId('id_customer')->references('id')->on('customers')->onDelete('cascade');
+//        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
