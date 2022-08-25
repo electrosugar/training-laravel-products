a1c853f73205fefc4e28838d13411db57b7763fb
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-25 16:53:09 +0300

    13 Cart http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/13

--- training-laravel-products/app/Models/Product.php
+++ training-laravel-products/app/Models/Product.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+use function Symfony\Component\String\__construct;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $primaryKey = 'id';
 
     public $timestamps = false;
     protected $fillable = [
@@ -19,32 +22,44 @@ class Product extends Model
         'price',
         'image_path'
     ];
-    public $id;
-    public $title;
-    public $description;
-    public $price;
-    public $image_path;
+//    public $id;
+//    public $title;
+//    public $description;
+//    public $price;
+//    public $image_path;
 
-    public function __construct($id, $title, $description, $price, $image_path)
+
+    public static function create($properties)
     {
-        $this->id = $id;
-        $this->title = $title;
-        $this->description = $description;
-        $this->price = $price;
-        $this->image_path = $image_path;
-        return $this;
-    }
-
-    public static function find($id)
-    {
-        $product = DB::table('products')->select('*')->where('id', $id)->get();
-
-        if (!$product) {
-            throw new ModelNotFoundException();
-        }
-
+        $product = new Product();
+        $product->id = $properties->id;
+        $product->title = $properties->title;
+        $product->description = $properties->description;
+        $product->price = $properties->price;
+        $product->image_path = $properties->image_path;
         return $product;
     }
+
+//    public function save(array $options = [])
+//    {
+//        DB::table('products')->insert([
+//            'title' => $this->title,
+//            'description' => $this->description,
+//            'price' => $this->price,
+//            'image_path' => $this->image_path
+//        ]);
+//    }
+
+//    public static function find($id)
+//    {
+//        $product = DB::table('products')->select('*')->where('id', $id)->get();
+//
+//        if (!$product) {
+//            throw new ModelNotFoundException();
+//        }
+//
+//        return $product;
+//    }
 
     public static function allProducts()
     {
@@ -60,12 +75,8 @@ class Product extends Model
     public static function notInCart()
     {
         if (Session::has('cart')) {
-            $indexProducts = DB::table('products')->select('*')->whereNotIn('id', Session::get('cart'))->get();
+            $indexProducts = Product::whereNotIn('id', Session::get('cart'))->get();
         } else {
-            $indexProducts = DB::table('products')->select('*')->get();
-        }
-
-        if (!$indexProducts) {
             throw new ModelNotFoundException();
         }
 
@@ -75,16 +86,16 @@ class Product extends Model
     public static function inCart()
     {
         if (Session::has('cart')) {
-            $indexProducts = DB::table('products')->select('*')->whereIn('id', Session::get('cart'))->get();
+            $cartProducts = Product::whereIn('id', Session::get('cart'))->get();
         } else {
-            $indexProducts = DB::table('products')->select('*')->get();
-        }
-
-        if (!$indexProducts) {
             throw new ModelNotFoundException();
         }
 
-        return $indexProducts;
+        if (!$cartProducts) {
+            throw new ModelNotFoundException();
+        }
+
+        return $cartProducts;
 
     }
 
--- training-laravel-products/resources/views/cart.blade.php
+++ training-laravel-products/resources/views/cart.blade.php
@@ -20,16 +20,22 @@
             <button id="btn-submit" type="submit">Remove from cart</button>
         </form>
     @endforeach
-    <form action="{{url('cart')}}" method="POST" id="product-remove">
+    <h1>{{__('Submit Order')}}</h1>
+    <form action="{{url('cart/order')}}" method="post"
+          class="form-group">
         @csrf
-
-        <button id="btn-submit" type="submit">Remove from cart</button>
+        <input type="text" name="name" placeholder="{{__('Name') }}"><br>
+        <input type="text" name="contact" placeholder="{{__('Contact') }}"
+               value="{{$value = isset($_POST['price'])?$_POST['price']:''}}"><br>
+        <textarea rows="4" cols="50" name="comment" placeholder="{{__('Comment') }}">
+        </textarea><br>
+        <span class="formLinks"> <button type="submit">{{__('Checkout')}}</button></span>
     </form>
     <a href="{{ url('index') }}">Go to index</a>
     <script type="text/javascript">
         $(document).ready(function () {
             $("#product-remove").submit(function (e) {
-                $("#btn-submit").attr("disabled", true);
+                $("#btn-submit").attr('disabled', true);
                 return true;
             });
         });
