c71a9712eaf4de6e6d6afda3c4ce0c4123ac4a63
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-25 16:52:16 +0300

    11 Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/11

--- training-laravel-products/database/factories/UserFactory.php
+++ training-laravel-products/database/factories/UserFactory.php
@@ -18,25 +18,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'username' => 'electrosugar',
+            'password' => '$2a$12$c8ykswn36fA8/NP5sFV5W.hxtUSkXIWrIVHLjvsZ4eMExlHGJC18q' // password
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
 }
