ecce4dd52d592335218abbaafe89a06642e19b23
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-25 16:54:28 +0300

    14 Login http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/14 login controller

--- training-laravel-products/app/Http/Controllers/LoginController.php
+++ training-laravel-products/app/Http/Controllers/LoginController.php
@@ -11,34 +11,34 @@ use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('login');
     }
 
-    public function login(){
-
-
+    public function login()
+    {
         $credentials = request()->validate([
-           'username'=>'required',
-           'password'=> 'required',
-       ]);
+            'username' => 'required',
+            'password' => 'required',
+        ]);
 
 
         //successful auth
 
-       if(Auth::attempt($credentials)){
-           return redirect('/products')->with('success', 'Welcome Back!');
-       }
+        if (Auth::attempt($credentials)) {
+            return redirect('/products')->with('success', 'Welcome Back!');
+        }
         return back()
-           ->withInput()
-           ->withErrors([
-           'username' => 'Your provided credentials could not be verified'
-       ]);
-
+            ->withInput()
+            ->withErrors([
+                'username' => 'Your provided credentials could not be verified'
+            ]);
 
     }
 
-    public function logout(){
+    public function logout()
+    {
         \auth()->logout();
 
         return redirect('/login')->with('success', 'Goodbye');
