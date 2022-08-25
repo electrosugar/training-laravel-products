1a589c7f13935ff63ada4b11252eebfb2835aea5
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-25 16:52:24 +0300

    11 Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/11

---
+++ training-laravel-products/database/factories/CustomerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=> $this->faker->name(),
+            'contact'=> $this->faker->email(),
+            'comment'=>$this->faker->paragraph(3, 2),
+            'creation_date'=>$this->faker->date('Y-m-d-h-m-s')
+        ];
+    }
+}
