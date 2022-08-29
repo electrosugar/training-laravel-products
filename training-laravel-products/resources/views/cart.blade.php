128fcc78433bbdc91d0a629d58f94bc041428bd1
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-29 16:54:06 +0300

    13 Cart http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/13 added success message for checkout

--- training-laravel-products/resources/views/cart.blade.php
+++ training-laravel-products/resources/views/cart.blade.php
@@ -22,7 +22,12 @@
     @endforeach
     <h1>{{__('Submit Order')}}</h1>
     <form action="{{url('cart/order')}}" method="post"
-          class="form-group">
+          class="form-group" id="product-remove">
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -36,7 +41,7 @@
         <input type="text" name="name" placeholder="{{__('Name') }}" value="{{old('name')}}"><br>
         <input type="text" name="contact" placeholder="{{__('Contact') }}" value="{{old('contact')}}"><br>
         <textarea rows="4" cols="20" name="comment" placeholder="{{__('Comment') }}">{{old('comment')}}</textarea><br>
-        <span class="formLinks"> <button type="submit">{{__('Checkout')}}</button></span>
+        <span class="formLinks"> <button type="submit" id="btn-submit">{{__('Checkout')}}</button></span>
     </form>
     <a href="{{ url('index') }}">{{__('Go to index')}}</a>
     <a href="{{ url('orders') }}">{{__('Go to orders')}}</a>
--- training-laravel-products/resources/views/layout.blade.php
+++ training-laravel-products/resources/views/layout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <title>Show Products</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="/app.css">
 </head>
 <body>
 
