2536d8381fa517766497349789d31c2a0b21bf3e
electrosugar <razvan.mocanu@5psolutions.eu>
2022-09-08 17:42:49 +0300

    10: PageController http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/10 added a controller to obtain all pages for routes

---
+++ training-laravel-products/app/Http/Controllers/PageController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\OrderProduct;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function order($id)
+    {
+        $order = Order::findOrFail($id);
+        $order->total_price = OrderProduct::where('order_id', $id)->sum('price');
+        return view('order', ['order' => $order]);
+    }
+
+    public function orders()
+    {
+        $orders = [];
+        foreach (Order::all() as $order){
+            $order->total_price = OrderProduct::where('order_id', $order->id)->sum('price');
+            $orders[] = $order;
+        }
+        return view('orders', ['orders' => $orders]);
+    }
+
+    public function index()
+    {
+        return view('index', ['products' => (Product::notInCart())]);
+    }
+
+    public function products()
+    {
+        return view('products', ['products' => Product::all()]);
+    }
+
+    public function product($id=null)
+    {
+        if (isset($id)) {
+            $product = Product::findOrFail($id);
+            return view('product', ['product' => $product]);
+
+        } else {
+            return view('product');
+
+        }
+    }
+
+    public function cart()
+    {
+        return view('cart', ['products' => Product::inCart(), 'displayMail' => false]);
+    }
+}
--- training-laravel-products/routes/web.php
+++ training-laravel-products/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
 use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
@@ -16,27 +17,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/index', [ProductController::class, 'index']);
+Route::get('/index', [PageController::class, 'index']);
 Route::post('/index', [ProductController::class, 'add']);
 
-Route::get('/cart', [ProductController::class, 'cart'])->middleware('guest');
-Route::post('/cart', [ProductController::class, 'remove'])->middleware('guest');
-Route::post('/cart/order', [OrderController::class, 'checkout'])->middleware('guest');
+Route::get('/cart', [PageController::class, 'cart']);
+Route::post('/cart', [ProductController::class, 'remove']);
+Route::post('/cart/order', [OrderController::class, 'checkout']);
 
-Route::get('/login', [LoginController::class, 'create'])->name('login')->middleware('guest');
+Route::get('/login', [PageController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 
-Route::get('/products', [ProductController::class, 'products'])->middleware('auth');
+Route::get('/products', [PageController::class, 'products'])->middleware('auth');
 Route::post('/products', [ProductController::class, 'delete'])->middleware('auth');
 
-Route::get('/product/{id?}', [ProductController::class, 'product'])->whereNumber('id')->middleware('auth');
+Route::get('/product/{id?}', [PageController::class, 'product'])->whereNumber('id')->middleware('auth');
 Route::post('/product/{id}', [ProductController::class, 'edit'])->middleware('auth');
 Route::post('/product', [ProductController::class, 'insert'])->middleware('auth');
 
-Route::get('/order/{id}', [OrderController::class, 'order'])->whereNumber('id');
+Route::get('/order/{id}', [PageController::class, 'order'])->whereNumber('id');
 
-Route::get('/orders', [OrderController::class, 'orders'])->middleware('auth');
+Route::get('/orders', [PageController::class, 'orders'])->middleware('auth');
 
 
 
