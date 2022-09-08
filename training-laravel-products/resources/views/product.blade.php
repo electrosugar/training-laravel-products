f0d0653e4b046d970aded7601ea15bcb952b89b5
electrosugar <razvan.mocanu@5psolutions.eu>
2022-09-08 17:45:59 +0300

    16: Product http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/16 success message added

--- training-laravel-products/resources/views/product.blade.php
+++ training-laravel-products/resources/views/product.blade.php
@@ -1,30 +1,33 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{isset($id) ? __('Editing Product #') . $id : __('Creating new product')}}</h1>
-    <form enctype="multipart/form-data" action="{{url('product/' . $value = isset($id) ? $id : '')}}" method="post"
+    <h1>{{isset($product->id) ? __('Editing Product #') . $product->id : __('Creating new product')}}</h1>
+    <form enctype="multipart/form-data" action="{{url('product/' . $value = isset($product->id) ? $product->id : '')}}" method="post"
           class="form-group">
-        @if(session()->has('message'))
-            <div class="alert success">
-                {{ session()->get('message') }}
+        @if(session()->has('success'))
+            <div class="success">
+                {{ session()->get('success') }}
             </div>
         @endif
         @csrf
-        <input type="text" name="title" placeholder="{{__('Title') }}" value="{{old('title')}}"><br>
+        <input type="text" name="title" placeholder="{{__('Title') }}" value="{{old('title') ?? $product->title ?? ''}}"><br>
             @error('title')
             <div class="error"> {{$message}} </div>
             @enderror
         <input type="text" name="description" placeholder="{{__('Description') }}"
-               value="{{old('description')}}"><br>
+               value="{{old('description') ?? $product->description ?? ''}}"><br>
             @error('description')
             <div class="error"> {{$message}} </div>
             @enderror
         <input type="text" name="price" placeholder="{{__('Price') }}"
-               value="{{old('price')}}"><br>
+               value="{{old('price') ?? $product->price ?? ''}}"><br>
             @error('price')
             <div class="error"> {{$message}} </div>
             @enderror
         <input type="file" name="image"><br>
+            @if(isset($product))
+            <img src="{{asset($product->image_path )}}" alt="{{$product->image_path}}">
+            @endif
             @error('image')
             <div class="error"> {{$message}} </div>
             @enderror
